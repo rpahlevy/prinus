200a4da8d898834cb4eb846fe035c9ddc79f6d93
rpahlevy <43176811+rpahlevy@users.noreply.github.com>
2020-07-04 11:31:50 +0700

    add produk primabase

--- src/routes/main.php
+++ src/routes/main.php
@@ -18,40 +18,40 @@ $produk = [
         'desc' => 'Paket software yang berfungsi untuk menerima data periodik dari primaGateway dan fungsi manajemen data Hujan dan Tinggi Muka Air serta data pendukung.',
         'page' => 'primabase.html',
     ],
-    'primaled' => [
-        'name' => 'primaLED',
-        'slug' => 'primaled',
-        'image' => '',
-        'desc' => 'LED untuk menampilkan informasi TMA, berikut fungsi alarm jika terjadi kenaikan ke tingkat Siaga.',
-        'page' => 'primaled.html',
-    ],
     'primagateway' => [
         'name' => 'primaGateway',
         'slug' => 'primagateway',
         'image' => '',
         'desc' => 'Penyimpanan data telemetri di Internet, anda tidak perlu menyediakan tenaga ahli bidang Internet dan Hosting.',
-        'page' => 'primagateway.html',
+        'page' => '',
+    ],
+    'primaled' => [
+        'name' => 'primaLED',
+        'slug' => 'primaled',
+        'image' => '',
+        'desc' => 'LED untuk menampilkan informasi TMA, berikut fungsi alarm jika terjadi kenaikan ke tingkat Siaga.',
+        'page' => '',
     ],
     'primacam' => [
         'name' => 'primaCam',
         'slug' => 'primacam',
         'image' => '',
         'desc' => 'Ada kalanya anda perlu merekam gambar pada titik lokasi kritis dari sungai atau obyek yang perlu direkam dalam bentuk gambar.',
-        'page' => 'primacam.html',
+        'page' => '',
     ],
     'primaconsole' => [
         'name' => 'primaConsole',
         'slug' => 'primaconsole',
         'image' => '',
         'desc' => 'Adalah alat bantu untuk mendiagnosa masalah yang terjadi pada Logger primaBot.',
-        'page' => 'primaconsole.html',
+        'page' => '',
     ],
     'primaweb' => [
         'name' => 'primaWeb',
         'slug' => 'primaweb',
         'image' => '',
         'desc' => 'Adalah alat bantu untuk mendiagnosa masalah yang terjadi pada Logger primaBot.',
-        'page' => 'primaweb.html',
+        'page' => '',
     ],
 ];
 
@@ -61,7 +61,8 @@ $app->get('[/]', function (Request $request, Response $response, $args) use ($pr
     ]);
 });
 
-$app->get('/produk/{slug}', function (Request $request, Response $response, $args) use ($produk) {
+$app->redirect('/produk[/]', '/#produk');
+$app->get('/produk/{slug}[/]', function (Request $request, Response $response, $args) use ($produk) {
     $slug = $args['slug'];
     if (!isset($produk[$slug])) {
         throw new \Slim\Exception\NotFoundException($request, $response);
